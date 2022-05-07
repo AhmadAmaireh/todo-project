7bc0d26eadab4179d863dbecbe1ef6039e05f56f
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-08 01:09:26 +0300

    todo-js

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+var userNmae = window.prompt("Enter your name");
+console.log(userNmae);
+
+
+
+var userGender = window.prompt("Enter your gender");
+if (userGender = 'male'){
+    console.log("Welcome Mr"+ userGender);
+}
+else{
+    console.log("Welcome Ms"+ userGender);
+}
+
+var userAge = window.prompt("Enter your age");
+if (userAge > 0) {
+    console.log (userAge);
+}
+else{
+    console.log("The Age is Erorr");
+
+}
+
