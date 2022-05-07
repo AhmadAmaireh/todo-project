6142d94e1d34271eef4bd17a418764ae29e5df56
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-08 01:31:27 +0300

    todo-js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-
-
 var username =prompt('What is your name?');
 var gender =prompt('please enter your gender');
 if (gender.toLocaleLowerCase()=="male" || gender.toLocaleLowerCase()=="female" ){
     alert('accepted vale.');
 }
 
-else{alert ('unaccepte value!');};
+else{alert ('unaccepte value!');
+};
 
 var age=prompt('please enter your age');
 if (age<=0) {alert('age is less than or equal to zero!')}
-else {alert('age is reasonable');}; 
+else {alert('age is reasonable');
+}; 
 
 var conf=confirm('do you want to skip the welcoming message?');
 
@@ -24,4 +24,5 @@ if (gender.toLocaleLowerCase()=="male") {
   } else if(gender.toLocaleLowerCase()=="female"){
     alert('welcome Ms'+"  "   +username);
   }
-  else{alert('welcome'+' '+username);};
+  else{alert('welcome'+' '+username);
+};
